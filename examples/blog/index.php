f1672995b2702a878d62ef760f300169c5c7f3dc
jumblesale <rob@robsollars.co.uk>
2013-07-11 16:07:43 +0100

    Added basic new post page

--- examples/blog/index.php
+++ examples/blog/index.php
@@ -21,10 +21,37 @@ $app->renderer()
 		]),
 	]);
 
+$post = new \Phrototype\Validator();
+	$post->form()->method('post')->action('/posts/add')
+		->attributes(['class' => 'pure-form pure-form-stacked'])
+		->submit('Post!', ['class' => 'pure-button pure-button-primary']);
+	$post->group('post', 'Post')->field('title')
+		->description('Post title');
+	$post->group('post')->field('content')->type('text')
+		->description('Post details');
+	$post->group('author', 'Author')->field('name')
+		->description('Author name');
+	$post->group('author')->field('href')
+		->description('Website');
+
 $app->router()->get('/posts', function() use($app) {
 	return $app->render(
 		$app->read('views/post.mustache'),
-		['posts' => Model\Factory::load('examples/blog/data/posts.json')]
+		['posts' => Model\Factory::load('examples/blog/data/posts.json')->models()]
+	);
+});
+
+$app->router()->get('/posts/add', function() use($app, $post) {
+	return $app->render(
+		'{{{form}}}',
+		['form' => $post->html()]
+	);
+});
+
+$app->router()->post('/posts/add', function() use($app, $post) {
+	return $app->render(
+		'<h2>data:</h2><pre>{{data}}</pre>',
+		['data' => print_r($_POST, true)]
 	);
 });
 
