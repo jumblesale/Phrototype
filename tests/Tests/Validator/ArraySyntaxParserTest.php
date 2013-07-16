4d60b6a0b92db82e331923e789ed60d807cd8b47
jumblesale <rob@robsollars.co.uk>
2013-07-16 13:34:01 +0100

    Added method to allow text arrays to map to POST vars

---
+++ tests/Tests/Validator/ArraySyntaxParserTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Phrototype\Tests;
+
+use Phrototype\Validator\ArraySyntaxParser;
+
+class ArraySyntaxParserTest extends \PHPUnit_Framework_TestCase {
+	public function setUp() {
+		$this->parser = new ArraySyntaxParser();
+	}
+
+	public function testErrantKey() {
+		$this->assertNull(
+			$this->parser->parse(
+				[],
+				'depth-1'
+			)
+		);
+	}
+
+	public function testSingleLevelArray() {
+		$this->assertEquals(
+			3,
+			$this->parser->parse(
+				['depth-1' => 3],
+				'depth-1'
+			)
+		);
+	}
+
+	public function testSingleDepth() {
+		$this->assertEquals(
+			5,
+			$this->parser->parse(
+				['sea' => ['monster' => 5]],
+				'sea[monster]'
+			)
+		);
+	}
+
+	public function testComplexData() {
+		$this->assertEquals(
+			11,
+			$this->parser->parse(
+				['sea' => [
+					'monster' => 5,
+					'horse' => [
+						'shoe' => 12,
+					],
+					'pony' => [
+						'trek' => [
+							'star' => 11
+						]
+					]
+				]],
+				'sea[pony][trek][star]'
+			)
+		);
+	}
+}
